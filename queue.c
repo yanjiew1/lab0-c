9731fdb095cd53c78f4aaa190e0609e22cf439d4
Yan-Jie Wang <yanjiewtw@gmail.com>
2023-02-15 16:18:51 +0800

    Implement 'q_size()'

--- queue.c
+++ queue.c
@@ -120,7 +120,13 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    int count = 0;
+    struct list_head *it;
+
+    list_for_each (it, head)
+        count++;
+
+    return count;
 }
 
 /* Delete the middle node in queue */
