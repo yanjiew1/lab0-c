867baff5bda32861acc7dfe8b37ee943f8e144d6
Yan-Jie Wang <yanjiewtw@gmail.com>
2023-02-17 10:39:33 +0800

    Implement q_delete_dup

--- queue.c
+++ queue.c
@@ -171,6 +171,31 @@ bool q_delete_mid(struct list_head *head)
 bool q_delete_dup(struct list_head *head)
 {
     // https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+    if (!head)
+        return false;
+
+    LIST_HEAD(trash);
+    element_t *it, *safe;
+    struct list_head *cut = head;
+
+    list_for_each_entry_safe (it, safe, head, list) {
+        if (&safe->list != head && strcmp(safe->value, it->value) == 0)
+            continue;
+        /* Detect duplicated elements */
+        if (it->list.prev != cut) {
+            LIST_HEAD(tmp);
+            list_cut_position(&tmp, cut, &it->list);
+            list_splice(&tmp, &trash);
+        }
+        cut = safe->list.prev;
+    }
+
+    /* empty trash */
+    list_for_each_entry_safe (it, safe, &trash, list) {
+        list_del(&it->list);
+        q_release_element(it);
+    }
+
     return true;
 }
 
