7605e680e3d82a4765df126807f878c146f20a3a
Yan-Jie Wang <yanjiewtw@gmail.com>
2023-02-19 07:14:02 +0800

    Make `q_copy_string` more compact
    Refactor `q_copy_string` to make it more compact.
    Now, it requires that size is positive.

--- queue.c
+++ queue.c
@@ -100,7 +100,7 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
     element_t *elem = list_first_entry(head, element_t, list);
     list_del(&elem->list);
 
-    if (sp)
+    if (sp && bufsize)
         q_copy_string(sp, bufsize, elem->value);
 
     return elem;
@@ -115,7 +115,7 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
     element_t *elem = list_last_entry(head, element_t, list);
     list_del(&elem->list);
 
-    if (sp)
+    if (sp && bufsize)
         q_copy_string(sp, bufsize, elem->value);
 
     return elem;
